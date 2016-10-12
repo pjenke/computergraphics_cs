3ba4ec9dca24741e2ea65e3581cf4df1826fad2d
xXMalcolmXx <maximilian.reinhardt@haw-hamburg.de>
2016-10-12 13:19:37 +0200

    Added Project SPH

---
+++ computergraphics/computergraphics/src/projects/sph/Core.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+
+namespace computergraphics
+{
+    class Core : LeafNode
+    {
+        public override void DrawGL(RenderMode mode, Matrix4 modelMatrix)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void TimerTick(int counter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ computergraphics/computergraphics/src/projects/sph/SPHScene.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace computergraphics
+{
+    class SPHScene : Scene
+    {
+
+        public SPHScene() : base(100, Shader.ShaderMode.PHONG, RenderMode.REGULAR)
+        {
+
+        }
+
+    }
+}
